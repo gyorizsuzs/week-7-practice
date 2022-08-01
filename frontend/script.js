01400dd416ecbdf2a2103449da54ba759ddee87c
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-01 21:18:15 +0200

    sex select added

--- frontend/script.js
+++ frontend/script.js
@@ -8,24 +8,37 @@
 
 let formComponent = function () {
   return `
-    <form>
-        <input id="name-input" type="text" placeholder="Enter your name..." />
-    </form>
-`;
+  <form>
+  <input id="name-input" type="text" placeholder="Enter your name..." />
+  </form>
+  `;
 };
 
 let displayComponent = function () {
   return `
-        <p id="name-display">Your Name</p>
-    `;
+  <p id="name-display">Your Name</p>
+  `;
 };
 
-let contentComponent = function (first, second) {
+let selectComponent = function () {
   return `
-        <section class="first">${first}</section>
-        <section class="second">${second}</section>
-    `;
+    <label id="sex-select">Sex:<br/>
+      <select name="sex-select:drop-down">
+        <option value="1">female</option>
+        <option value="2">male</option>
+        <option value="3">other</option>
+      </select>
+  `;
+}
+
+let contentComponent = function (first, second, third) {
+  return `
+  <section class="first">${first}</section>
+  <section class="second">${second}</section>
+  <section class="third">${third}</section>
+  `;
 };
+
 
 /* === Initialize Functions === */
 
@@ -36,26 +49,39 @@ function loadEvent() {
 
   rootElement.insertAdjacentHTML(
     "beforeend",
-    contentComponent(formComponent(), displayComponent())
+    contentComponent(formComponent(), displayComponent(), selectComponent())
   );
 
   document.querySelector("form").addEventListener("submit", function (event) {
     event.preventDefault();
   });
-  
+
   let inputElement = document.querySelector("#name-input");
-  
+
   const displayElement = document.querySelector("#name-display");
-  
+
+  let selectElement = document.querySelector("#sex-select");
+
   inputElement.addEventListener("input", function (event) {
     let inputData = event.target.value;
     displayElement.innerHTML = inputData;
-    console.log(event.target.value);
+    console.log(inputData);
   });
+
+  selectElement.addEventListener("option", function (event) {
+    let optionData = event.target.value;
+    displayElement.innerHTML = optionData;
+    console.log(optionData);
+  })
 }
+
+
 
 /* === Call Functions === */
 
 window.addEventListener(`load`, loadEvent);
 
-/* feladat, adni a selectnek eventlistenert, https://www.w3schools.com/tags/tag_select.asp lenyilo gecit kell baszni ra faszom */
+/* feladat, adni a selectnek eventlistenert, https://www.w3schools.com/tags/tag_select.asp  */
+
+
+
