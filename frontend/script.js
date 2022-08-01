1929c1cd2f47e5d0093752dbccb72fcf6375c7b3
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-01 17:19:56 +0200

    event listeners

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,60 @@
 /*===============================================================*\
 *                                                                 * 
-*                          JS HOISTING                            *
+*                          JS EVENT LISTENERS                     *
 *                                                                 * 
 \*================================================================*/
 
+/* === Initialize Components === */
 
-/* function testFunction() {
-    const x= "Rammstein";
-    console.log(x);
-} */
+let formComponent = function () {
+  return `
+    <form>
+        <input id="nameInput" type="text" placeholder="Enter your name..." />
+    </form>
+`;
+};
 
-const up = function() {
-    const x= "Rammstein";
-    console.log(x);
+let displayComponent = function () {
+  return `
+        <p id="name-display">Your Name</p>
+    `;
+};
+
+let contentComponent = function (first, second) {
+  return `
+        <section class="first">${first}</section>
+        <section class="second">${second}</section>
+    `;
+};
+
+/* === Initialize Functions === */
+
+function loadEvent() {
+  console.log('loadEvent executed');
+
+  let rootElement = document.querySelector('#root');
+
+  rootElement.insertAdjacentHTML(
+    'beforeend',
+    contentComponent(formComponent(), displayComponent())
+  );
+
+  document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+  });
+
+  let inputElement = document.querySelector('#name-input');
+
+  const displayElement = document.querySelector('#name-display');
+
+  inputElement.addEventListener('input', function (event) {
+    let inputData = event.target.value;
+    displayElement.innerHTML = inputData;
+  });
 }
 
-up();
+/* === Call Functions === */
+
+window.addEventListener(`load`, loadEvent);
+
+/* feladat, adni a selectnek eventlistenert, https://www.w3schools.com/tags/tag_select.asp lenyilo gecit kell baszni ra faszom */
