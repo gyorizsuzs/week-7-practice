f2611954716cacccba83dec997ac2f8d8cbcc6db
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-01 22:10:13 +0200

    creating form

--- frontend/script.js
+++ frontend/script.js
@@ -9,33 +9,91 @@
 let formComponent = function () {
   return `
   <form>
-  <input id="name-input" type="text" placeholder="Enter your name..." />
+  <input id="name-input" type="text" placeholder="Character Name" />
+  <br>
+  <label id="gender-select">Gender:<br/>
+      <select name="gender-select:drop-down">
+        <option value="1">female</option>
+        <option value="2">male</option>
+        <option value="3">other</option>
+      </select>
+    <br>
+  <label id="race-select">Race:<br/>
+    <select name="race-select:drop-down">
+      <option value="1">Ainur</option>
+      <option value="2">Black Númenórean</option>
+      <option value="3">Dwarf</option>
+      <option value="4">Elf</option>
+      <option value="5">Hobbit</option>
+      <option value="6">Maiar</option>
+      <option value="7">Men</option>
+      <option value="8">Nazgûl</option>
+      <option value="9">Olog-Hai</option>
+      <option value="9">Undead</option>
+      <option value="10">Uruk</option>
+      <option value="10">Valar</option>
+    </select>
+  <br>
+  <label id="class-select">Class:<br/>
+    <select name="class-select:drop-down">
+      <option value="1">Beastmaster</option>
+      <option value="2">Executioner</option>
+      <option value="3">Ranger</option>
+      <option value="4">Slaver</option>
+      <option value="5">Sawbone</option>
+    </select>
   </form>
   `;
 };
 
 let displayComponent = function () {
   return `
-  <p id="name-display">Your Name</p>
+  <p id="name-display">Character</p>
   `;
 };
 
-let selectComponent = function () {
+/* let selectComponent = function () {
   return `
-    <label id="sex-select">Sex:<br/>
-      <select name="sex-select:drop-down">
+    <label id="gender-select">Gender:<br/>
+      <select name="gender-select:drop-down">
         <option value="1">female</option>
         <option value="2">male</option>
         <option value="3">other</option>
       </select>
-  `;
-}
+    <br>
+    <label id="race-select">Race:<br/>
+    <select name="race-select:drop-down">
+      <option value="1">Ainur</option>
+      <option value="2">Black Númenórean</option>
+      <option value="3">Dwarf</option>
+      <option value="4">Elf</option>
+      <option value="5">Hobbit</option>
+      <option value="6">Maiar</option>
+      <option value="7">Men</option>
+      <option value="8">Nazgûl</option>
+      <option value="9">Olog-Hai</option>
+      <option value="9">Undead</option>
+      <option value="10">Uruk</option>
+      <option value="10">Valar</option>
+      </select>
+    <br>
+    <label id="class-select">Class:<br/>
+    <select name="class-select:drop-down">
+      <option value="1">Beastmaster</option>
+      <option value="2">Executioner</option>
+      <option value="3">Ranger</option>
+      <option value="4">Slaver</option>
+      <option value="5">Sawbone</option>
+    </select>
+      `;
+    } */
+
 
 let contentComponent = function (first, second, third) {
   return `
   <section class="first">${first}</section>
   <section class="second">${second}</section>
-  <section class="third">${third}</section>
+ 
   `;
 };
 
@@ -49,7 +107,7 @@ function loadEvent() {
 
   rootElement.insertAdjacentHTML(
     "beforeend",
-    contentComponent(formComponent(), displayComponent(), selectComponent())
+    contentComponent(formComponent(), displayComponent(),)
   );
 
   document.querySelector("form").addEventListener("submit", function (event) {
@@ -60,7 +118,7 @@ function loadEvent() {
 
   const displayElement = document.querySelector("#name-display");
 
-  let selectElement = document.querySelector("#sex-select");
+ /*  let selectElement = document.querySelector("#gender-select"); */
 
   inputElement.addEventListener("input", function (event) {
     let inputData = event.target.value;
@@ -68,11 +126,11 @@ function loadEvent() {
     console.log(inputData);
   });
 
-  selectElement.addEventListener("option", function (event) {
+/*   selectElement.addEventListener("option", function (event) {
     let optionData = event.target.value;
     displayElement.innerHTML = optionData;
     console.log(optionData);
-  })
+  }) */
 }
 
 
